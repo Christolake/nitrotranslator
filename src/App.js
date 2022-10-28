f5bd11840f7c27683c8211fe9387cf2021d0f4a3
Christolake <the.christolake@gmail.com>
2022-10-28 13:56:02 -0400

    init. This is alpha 0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const languages = ['en', 'es', 'it']
+  const flags = ['🇺🇸 *English:*\n', '\n\n🇦🇷 *Spanish:*\n', '\n\n🇮🇹 *Italian:*\n']
+  let finalText = {en:'', es:'', it: ''};
+  const api = {
+    translate: 'https://translate.argosopentech.com/translate',
+    detect: 'https://translate.argosopentech.com/detect'
+  }
+
+  const [input, setInput] = useState('');
+  const handleInput = (e) => setInput(e.target.value);
+
+  const translate = async () => {
+  const getFinalText = 
+    languages.map(async lang => {
+      const response = await axios.post(api.translate, {
+        q: input,
+        source: 'auto',
+        target: lang
+      })
+      return finalText[lang] = response.data.translatedText
+      })
+    
+  
+    const textTranslated = await Promise.all(getFinalText)
+    const returnString = textTranslated.map((text, index) => `${flags[index]}${text}`);
+    return window.open(`whatsapp://send?text=${window.encodeURIComponent(returnString.join(''))}`)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Nitro Translator</h1>
       </header>
+      <main>
+        <textarea 
+        rows="10" 
+        cols="50"
+        onChange={handleInput}>
+        </textarea>
+        <button onClick={translate}>TRANSLATE</button>
+      </main>
     </div>
   );
 }
